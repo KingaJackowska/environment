3cf79f6b1178710e8b0cd1ecfabf6bf0f56277a8
Kinga Jackowska <jackowska.kinga@gmail.com>
2019-05-08 19:34:17 +0200

    commit message

---
+++ program.js
@@ -0,0 +1,23 @@
+process.stdin.setEncoding('utf-8');
+
+
+process.stdin.on('readable', function(){
+    var input = process.stdin.read();
+    var instruction = input.toString().trim();
+
+    switch (instruction) {
+        case '/exit':
+            process.exit();
+            break
+        case '/version':
+            process.stdout.write("Current verision: " + process.versions.node);
+            break
+        case '/language':
+            process.stdout.write("Language: " + process.env.LANG);
+            break
+
+        default:
+            process.stderr.write('Wrong instruction!\n');
+        
+    }
+});
